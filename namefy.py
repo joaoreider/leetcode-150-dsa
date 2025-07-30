adaacdf84e696ede8af3d9720c7d0c98a77d49b9
Joao Paulo <joaopauloj405@gmail.com>
2025-07-30 16:06:47 -0300

    add script to name my questions from title

---
+++ namefy.py
@@ -0,0 +1,11 @@
+import re
+
+def nameFile(text: str) -> str:
+
+    text = text.lower()
+    text = re.sub(r'[^a-z0-9\s-]', '', text)
+    text = re.sub(r'[\s-]+', '-', text)
+    text = text.strip('-') + '.py'
+    return text
+
+print(nameFile("Encode and Decode Strings"))
