46b307d61adeddf2ae2098354d45e55b859a639a
Joao Paulo <joaopauloj405@gmail.com>
2025-08-11 09:05:25 -0300

    evaluate-reverse-polish-notation

--- namefy.py
+++ namefy.py
@@ -8,4 +8,4 @@ def nameFile(text: str) -> str:
     text = text.strip('-') + '.py'
     return text
 
-print(nameFile("Encode and Decode Strings"))
+print(nameFile("Evaluate Reverse Polish Notation"))
